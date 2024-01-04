2c93a89332ce24148efc280c721c4ac6d3c44f4c
Clément <clementpages02@gmail.com>
2024-01-04 21:46:54 +0100

    Changement structure

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,23 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = 3000
+const mysql = require("mysql");
+
+//Initialisation de la base de données
+const con = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWD,
+    database: process.env.DATABASE_NAME
+});
 
 // Initialisation d'Express
 const app = express()
-
-console.log(process.env)
 app.listen(port, () => {
     console.log(`Server started on ${port}`)
 })
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
